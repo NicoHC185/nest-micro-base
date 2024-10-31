dbf79b27028b78805d706c531e05f3dfc5007e07
NicoHC185 <nikohidalgo.castillo@gmail.com>
2024-10-30 22:53:05 -0300

    ADD: append products crud

---
+++ src/config/envs.ts
@@ -0,0 +1,23 @@
+import "dotenv/config"
+import * as joi from 'joi';
+
+interface EnvsVars {
+    PORT: number
+}
+
+const envsSchema = joi.object({
+    PORT: joi.number().required()
+
+}).unknown(true)
+
+const { error, value } = envsSchema.validate(process.env)
+
+if (error) {
+    throw new Error(`Config validation error: ${error.message}`)
+}
+
+const envsVars: EnvsVars = value
+
+export const envs = {
+    port: envsVars.PORT
+}
